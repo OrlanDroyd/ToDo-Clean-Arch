3e4afb1bdfc5f0a33dfe7159e392278d5b1e895f
orlando.ppena <orlando.ppena@reduc.edu.cu>
2021-10-08 12:07:36 -0400

    Fix padding values

--- app/src/main/java/com/gmail/orlandroyd/notasnow/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/gmail/orlandroyd/notasnow/feature_note/presentation/notes/NotesScreen.kt
@@ -49,7 +49,11 @@ fun NotesScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .padding(
+                    top = 16.dp,
+                    end = 16.dp,
+                    start = 16.dp
+                )
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
